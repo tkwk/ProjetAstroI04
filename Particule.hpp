687d95a86fbf0161c34881dbc921332ec486abfa
qandre <quentin.andre@ens-cachan.fr>
2015-12-05 23:26:43 +0100

    Modification de la classe Particule

--- Particule.hpp
+++ Particule.hpp
@@ -11,6 +11,7 @@ class Particule {
 		Vector<DIM> r;	// Position of the particule
 		Vector<DIM> v;	// Velocity of the particule
 		Vector<DIM> f;	// Force felt by the particle
+		Vector<DIM> fnext; // Force felt by the particle at time (n+1) -> useful for Leapfrog scheme
 	public:
 		Particule();
 		Particule(const real & mass);
@@ -19,6 +20,7 @@ class Particule {
 		real kineticEnergy();
 
 		friend class Euler;
+		friend class Leapfrog;
 };
 
 #endif
