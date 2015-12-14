e2a44d6ff2fd1b1686b65ecab360dce1f1973ba7
tkwk <antonin.penon@gmail.com>
2015-12-14 17:50:10 +0000

    Fin des corrections

--- Universe.cpp
+++ Universe.cpp
@@ -10,15 +10,13 @@ void Universe::gForce(Particule & p) {
 	int Np = Particules.size();
 	int idp = p.id();
 
-	for (int d=0; d<DIM; d++) {
-		p.f[d] = 0.0; 
+		p.f = Vector<DIM>(); 
 	
 		for (int id=0; id<Np; id++) {
 			if (idp != id) {
-				p.f[d] += - p.m * Particules[id].m * (p.r[d] - Particules[id].r[d]) / pow((p.r - Particules[id].r).norm(), 3); 
+                p.f = (p.m * Particules[id].m * (Particules[id].r-p.r).normalized())/(Particules[id].r - p.r).squaredNorm();
 			}
 		}
-	}	
 }
 
 void Universe::gForces() {
