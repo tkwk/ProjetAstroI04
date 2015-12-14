a1c8422ce95b3f32101a8fb5f929696ed43f2c3e
qandre <quentin.andre@ens-cachan.fr>
2015-12-14 16:11:13 +0100

    Implementation de la fonction gforce

--- Universe.cpp
+++ Universe.cpp
@@ -1,9 +1,31 @@
 #include "Universe.hpp"
+#include <cmath>
 
-Universe::Universe() {
-	N_particules = 0;
+
+Universe::Universe() {}
+
+
+void Universe::gForce(Particule & p) {
+	int Np = Particules.size();
+	int idp = p.id();
+
+	for (int d=0; d<DIM; d++) {
+		p.f[d] = 0.0; 
+	
+		for (int id=0; id<Np; id++) {
+			if (idp != id) {
+				p.f[d] += - p.m * Particules[id].m * (p.r[d] - Particules[id].r[d]) / pow((p.r - Particules[id].r).norm(), 3); 
+			}
+		}
+	}	
 }
 
-void Universe::gForce(Particule *p) {
 
+double Universe::potentialEnergy(const Particule &) const {
+	return 0.0;
+}
+
+
+double Universe::totalEnergy(const Particule &) const {
+	return 0.0;
 }
