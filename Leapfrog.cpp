40911ed80efe929a474af4d67be766138863a9a8
tkwk <antonin.penon@gmail.com>
2016-01-27 22:13:34 +0000

    retrait de commentaires inutiles

--- Leapfrog.cpp
+++ Leapfrog.cpp
@@ -2,17 +2,6 @@
 #include "Universe.hpp"
 #include "Particule.hpp"
 
-
-/*void Leapfrog::timeStep(Particule & p) {
-	double dt = this->dt;
-	
-	for (int d=0; d<DIM; d++) p->r[d] += dt * p->v[d] + 0.5 * dt*dt * p->f[d]/p->m;
-	
-	universe.gForce(p);
-	for (int d=0; d<DIM; d++) p->v[d] += 0.5 * dt * (p->f[d] + p->fnext[d])/p->m;
-}*/
-
-
 void Leapfrog::universeStep(Universe & u) {
 	double dt = this->dt;
 	int Np = u.Particules.size();
