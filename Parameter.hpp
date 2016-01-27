6787021b4cc14a712594e8662a4749e8bde6764d
tkwk <antonin.penon@gmail.com>
2016-01-27 23:09:57 +0000

    Correction de Parameter.hpp

--- Parameter.hpp
+++ Parameter.hpp
@@ -1,47 +1,43 @@
-#ifndef __PARAMETER_HPP__
-#define __PARAMETER_HPP__
+
+#ifndef __PARAMETER_H__
+#define __PARAMETER_H__
 
 #include <string>
 #include <fstream>
 #include <sstream>
 
 struct Parameter {
-    Parameter(const std::string &);
-    void readFromFile(const std::string &);
-
+    Parameter(const std::string &file) {
+        readFromFile(file);
+    }
+    void readFromFile(const std::string &filename) {
+        ifstream file(filename.c_str());
+        std::string line;
+        while(getline(file,line)) {
+            std::stringstream ss(line);
+            std::string left, right;
+            getline(ss,left,'=');
+            getline(ss,right,'=');
+            if(left=="dt") {
+                stringstream sss(right);
+                sss >> dt;
+            }
+            else if(left=="T") {
+                stringstream sss(right);
+                sss >> T;
+            }
+            else if(left=="scheme") {
+                stringstream sss(right);
+                sss >> scheme;
+            }
+        }
+        
+        file.close();
+    }
     double dt;
     double T;
     std::string scheme;
 };
-
-Parameter::Parameter(const std::string &filename) {
-    readFromFile(filename);
-}
-
-void Parameter::readFromFile(const std::string & filename) {
-   ifstream file(filename.c_str());
-   std::string line;
-   while(getline(file,line)) {
-        std::stringstream ss(line);
-        std::string left, right;
-        getline(ss,left,'=');
-        getline(ss,right,'=');
-        if(left=="dt") {
-            stringstream sss(right);
-            sss >> dt;
-        }
-        else if(left=="T") {
-            stringstream sss(right);
-            sss >> T;
-        }
-        else if(left=="scheme") {
-            stringstream sss(right);
-            sss >> scheme;
-        }
-   }
-
-   file.close();
-}
 
 #endif
 
