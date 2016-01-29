22f55aaa8e014c16665b943544bd103d1e7586ea
quentin andre <qandre@ensta.fr>
2016-01-29 15:54:06 +0100

    Correction du schema Leapfrog

--- Leapfrog.cpp
+++ Leapfrog.cpp
@@ -8,7 +8,7 @@ void Leapfrog::universeStep(Universe & u) {
 
 	for (int p=0; p<Np; p++) {
 		u.Particules[p].r = u.Particules[p].r + dt * u.Particules[p].v + 0.5 * dt*dt * u.Particules[p].f/u.Particules[p].m;
-		u.gForces(SCHEME_LEAPFROG);
+		u.gForce(u.Particules[p] ,SCHEME_LEAPFROG);
 		u.Particules[p].v = u.Particules[p].v + 0.5 * dt * (u.Particules[p].f + u.Particules[p].fnext)/u.Particules[p].m;
 		u.Particules[p].f = u.Particules[p].fnext;
 	}
